5493cc5c05e287ab4b54b0fdd0acaaa5ec3d385a
Ondřej Surý <ondrej@sury.org>
2011-05-16 16:19:56 +0200

    Small optimization in cron.d script (Courtesy of Marcus Cobden)

--- debian/php5-common.php5.cron.d
+++ debian/php5-common.php5.cron.d
@@ -4,4 +4,4 @@
 #  files, or 24 minutes if not defined.  See /usr/lib/php5/maxlifetime
 
 # Look for and purge old sessions every 30 minutes
-09,39 *     * * *     root   [ -x /usr/lib/php5/maxlifetime ] && [ -d /var/lib/php5 ] && find /var/lib/php5/ -depth -mindepth 1 -maxdepth 1 -type f -cmin +$(/usr/lib/php5/maxlifetime) -execdir sh -c 'test -z "$(fuser "$0")"' {} \; -delete
+09,39 *     * * *     root   [ -x /usr/lib/php5/maxlifetime ] && [ -d /var/lib/php5 ] && find /var/lib/php5/ -depth -mindepth 1 -maxdepth 1 -type f -cmin +$(/usr/lib/php5/maxlifetime) ! -execdir fuser -z {} \; -delete
