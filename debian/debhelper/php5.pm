084694d61854b4fea55e8bd1090536d2eebd34db
Ondřej Surý <ondrej@sury.org>
2013-07-08 13:54:54 +0200

    Add dh_phpize and dh_phpize_clean helpers

--- debian/debhelper/php5.pm
+++ debian/debhelper/php5.pm
@@ -3,6 +3,8 @@ use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
 
+insert_before("dh_auto_configure", "dh_phpize");
+insert_before("dh_clean", "dh_phpize_clean");
 insert_after("dh_install", "dh_php5");
 
 1;
