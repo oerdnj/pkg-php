56e40dafa95d5c695dbf48e876051c1cc202eedb
Ondřej Surý <ondrej@sury.org>
2013-07-09 14:53:58 +0200

    Drop dh_phpize scripts in favor of dh_phppear

--- debian/debhelper/php5.pm
+++ debian/debhelper/php5.pm
@@ -3,8 +3,6 @@ use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
 
-insert_before("dh_auto_configure", "dh_phpize");
-insert_before("dh_clean", "dh_phpize_clean");
 insert_after("dh_install", "dh_php5");
 
 1;
